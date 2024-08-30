30dd2e9282acf63cc6bab18d32e91bc294a26d2a
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-08-30 05:42:37 +0500

    feat: вынес логику фильтрации в отдельный хук useFilter и использовал в ContactListPage

--- src/pages/ContactListPage/ContactListPage.tsx
+++ src/pages/ContactListPage/ContactListPage.tsx
@@ -1,81 +1,35 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import { useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
-import { ContactDto } from 'src/types/dto/ContactDto'
-import { groupStore } from 'src/store/groupStore/groupStore'
-import {
-	FilterForm,
-	FilterFormValues
-} from 'src/components/FilterForm/FilterForm'
-import { EmptyContactsList } from 'src/components/EmptyContactsList'
-import styles from './contactListPage.module.scss'
+import { ContactListPageContent } from './ContactListPageContent'
 import { contactStore } from 'src/store/contactStore/contactStore'
-import { ContactCard } from 'src/components/ContactCard'
-import { EMPTY_STRING } from 'src/constants/variables'
+import { groupStore } from 'src/store/groupStore/groupStore'
+import { useFilter } from 'src/hooks/useFilter'
+import { FilterFormValues } from 'src/components/FilterForm/FilterForm'
 
 export const ContactListPage = observer(() => {
-	const [filteredContacts, setFilteredContacts] = useState<ContactDto[]>([])
-	const [filterValues, setFilterValues] = useState<Partial<FilterFormValues>>({
-		name: '',
-		groupId: ''
-	})
-
 	const contacts = contactStore.contactsData
 	const groups = groupStore.groupsData
+
+	const { filterValues, setFilterValues, filteredContacts } = useFilter(
+		contacts,
+		groups,
+		{} as Partial<FilterFormValues>
+	)
+
+	const handleSubmit = (values: Partial<FilterFormValues>) => {
+		setFilterValues(values)
+	}
 
 	useEffect(() => {
 		contactStore.getContacts()
 		groupStore.getGroups()
 	}, [])
 
-	const applyFilters = useCallback(() => {
-		let filtered = contacts
-
-		const nameFilter = filterValues.name || EMPTY_STRING
-
-		if (nameFilter) {
-			filtered = filtered.filter(contact =>
-				contact.name.toLowerCase().includes(nameFilter.toLowerCase())
-			)
-		}
-
-		if (filterValues.groupId) {
-			const groupContactIds =
-				groups.find(group => group.id === filterValues.groupId)?.contactIds ||
-				[]
-
-			filtered = filtered.filter(contact =>
-				groupContactIds.includes(contact.id)
-			)
-		}
-
-		setFilteredContacts(filtered)
-	}, [contacts, filterValues, groups])
-
-	useEffect(() => {
-		applyFilters()
-	}, [applyFilters])
-
-	const handleFilterSubmit = (values: Partial<FilterFormValues>) => {
-		setFilterValues(values)
-	}
-
 	return (
-		<div className={styles.contact_listPage}>
-			<div className={styles.filter_formContainer}>
-				<FilterForm
-					initialValues={filterValues}
-					onSubmit={handleFilterSubmit}
-				/>
-			</div>
-			{filteredContacts.length === 0 ? (
-				<EmptyContactsList />
-			) : (
-				<div className={styles.contact_cardsContainer}>
-					{filteredContacts.map(contact => (
-						<ContactCard key={contact.id} contact={contact} withLink />
-					))}
-				</div>
-			)}
-		</div>
+		<ContactListPageContent
+			filteredContacts={filteredContacts}
+			filter={filterValues}
+			onSubmit={handleSubmit}
+		/>
 	)
 })
