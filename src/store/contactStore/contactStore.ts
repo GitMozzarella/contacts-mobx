073e51a6eefbdf2f6b52a1bbf49b104f339401e6
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-08-30 05:32:13 +0500

    feat: contactStore data from api

--- src/store/contactStore/contactStore.ts
+++ src/store/contactStore/contactStore.ts
@@ -2,6 +2,7 @@ import { ContactDto } from 'src/types/dto/ContactDto'
 import { api } from '../api/api'
 import { isSuccessResponse, Response } from '../../types/response'
 import { makeAutoObservable } from 'mobx'
+import { v4 as uuidv4 } from 'uuid'
 
 export const contactStore = makeAutoObservable({
 	contactsData: [] as ContactDto[],
@@ -12,5 +13,23 @@ export const contactStore = makeAutoObservable({
 		if (isSuccessResponse(result)) {
 			this.contactsData = result.data
 		}
+	},
+
+	// Заглушка для добавления контакта
+	createContact(contact: Omit<ContactDto, 'id'>) {
+		const newContact: ContactDto = { id: uuidv4(), ...contact }
+		this.contactsData.push(newContact)
+	},
+
+	// Заглушка для обновления контакта
+	updateContact(id: string, updatedData: Partial<ContactDto>) {
+		this.contactsData = this.contactsData.map(contact =>
+			contact.id === id ? { ...contact, ...updatedData } : contact
+		)
+	},
+
+	// Заглушка для удаления контакта
+	deleteContact(id: string) {
+		this.contactsData = this.contactsData.filter(contact => contact.id !== id)
 	}
 })
