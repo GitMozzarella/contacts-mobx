67ad78491e67044bcbd84cbfe00d35c9852761ac
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-08-30 05:43:42 +0500

    refactor: удалил папку redux

--- src/redux/logActionMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { Middleware } from 'redux'
-import { logAction } from '../metrics/logAction'
-import { RootState } from './store'
-
-export const logActionMiddleware: Middleware<{}, RootState> =
-	storeAPI => next => action => {
-		logAction(action as any)
-		return next(action)
-	}
